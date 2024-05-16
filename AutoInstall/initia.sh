abf3a4e5d63fd1ab148f3ae5d2d3701bc2788a44
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-05-16 08:56:08 +0300

    Update initia.sh

--- AutoInstall/initia.sh
+++ AutoInstall/initia.sh
@@ -33,7 +33,7 @@ cd $HOME
 rm -rf initia
 git clone https://github.com/initia-labs/initia.git
 cd initia
-git checkout v0.2.12
+git checkout v0.2.14
 make build
 mkdir -p $HOME/.initia/cosmovisor/genesis/bin
 mv build/initiad $HOME/.initia/cosmovisor/genesis/bin/
