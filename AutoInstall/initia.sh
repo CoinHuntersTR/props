16669bd2e26b7c47821e10b1e1bd9d1b165eccdb
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-05-20 11:36:21 +0300

    Update initia.sh

--- AutoInstall/initia.sh
+++ AutoInstall/initia.sh
@@ -24,7 +24,7 @@ sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential gi
 
 echo -e "\e[1m\e[32m1. GO'yu yükleniyor... \e[0m"
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.21.10.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.22.3.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
