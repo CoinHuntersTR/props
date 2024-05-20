ed1ef87ae071ec6e25706dabbd094353ee0fec0d
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-05-20 11:44:35 +0300

    Update initia.sh

--- AutoInstall/initia.sh
+++ AutoInstall/initia.sh
@@ -73,8 +73,8 @@ initiad config set client chain-id initiation-1
 initiad config set client keyring-backend test
 initiad config set client node tcp://localhost:17957
 initiad init $MONIKER --chain-id initiation-1
-curl -Ls https://snapshots.kjnodes.com/initia-testnet/genesis.json > $HOME/.initia/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/initia-testnet/addrbook.json > $HOME/.initia/config/addrbook.json
+curl -Ls https://raw.githubusercontent.com/CoinHuntersTR/props/main/initia/genesis.json > $HOME/.initia/config/genesis.json
+curl -Ls https://raw.githubusercontent.com/CoinHuntersTR/props/main/initia/addrbook.json > $HOME/.initia/config/addrbook.json
 sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@initia-testnet.rpc.kjnodes.com:17959\"|" $HOME/.initia/config/config.toml
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit,0.01uusdc\"|" $HOME/.initia/config/app.toml
 sed -i \
@@ -87,8 +87,7 @@ sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.
 sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:17917\"%; s%^address = \":8080\"%address = \":17980\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:17990\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:17991\"%; s%:8545%:17945%; s%:8546%:17946%; s%:6065%:17965%" $HOME/.initia/config/app.toml
 
 echo -e "\e[1m\e[32m1. Download latest chain snapshot... \e[0m"
-curl -L https://snapshots.kjnodes.com/initia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
-[[ -f $HOME/.initia/data/upgrade-info.json ]] && cp $HOME/.initia/data/upgrade-info.json $HOME/.initia/cosmovisor/genesis/upgrade-info.json
+screen -dmS snapshot bash -c "curl -L https://snapshots.kjnodes.com/initia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia && [[ -f $HOME/.initia/data/upgrade-info.json ]] && cp $HOME/.initia/data/upgrade-info.json $HOME/.initia/cosmovisor/genesis/upgrade-info.json"
 
 echo -e "\e[1m\e[32m1. Start service and check the logs... \e[0m"
 sudo systemctl start initia.service && sudo journalctl -u initia.service -f --no-hostname -o cat
