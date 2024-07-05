118ded102b9e292d16c6e84a698abab191977830
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-07-05 12:51:06 +0300

    Update Airchains.sh

--- AutoInstall/Airchains.sh
+++ AutoInstall/Airchains.sh
@@ -1,78 +1,130 @@
 #!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/common.sh)
 
-# Moniker al
-read -p "Lütfen Moniker'ınızı girin: " MONIKER
+printLogo
 
-echo -e "\e[1m\e[32m1. Update && Upgrade... \e[0m"
-sudo apt update && sudo apt upgrade -y
+read -p "Enter WALLET name:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (for example 17, default port=26):" PORT
+echo 'export PORT='$PORT
 
-echo -e "\e[1m\e[32m1. Paketleri yükleniyor... \e[0m"
-sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make lz4 unzip ncdu -y
-
-echo -e "\e[1m\e[32m1. GO'yu yükleniyor... \e[0m"
-ver="1.21.5"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-
-echo "export PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> $HOME/.bash_profile
+# set vars
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export AIRCHAIN_CHAIN_ID="junction"" >> $HOME/.bash_profile
+echo "export AIRCHAIN_PORT="$PORT"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-echo -e "\e[1m\e[32m1. Binary dosyasını indiriliyor... \e[0m"
-wget https://github.com/airchains-network/junction/releases/download/v0.1.0/junctiond
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$AIRCHAIN_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$AIRCHAIN_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.21.6"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+cd $HOME
+wget -O junctiond https://github.com/airchains-network/junction/releases/download/v0.1.0/junctiond
 chmod +x junctiond
-sudo mv junctiond /usr/local/bin
+mv junctiond $HOME/go/bin/
 
-echo -e "\e[1m\e[32m1. Config, Addrbook ve Genesis dosyalarını ayarlanıyor... \e[0m"
-junctiond config chain-id junction
-junctiond init "$MONIKER" --chain-id junction
+printGreen "5. Configuring and init app..." && sleep 1
+# config and init app
+junctiond init $MONIKER --chain-id $AIRCHAIN_CHAIN_ID 
+sed -i -e "s|^node *=.*|node = \"tcp://localhost:${AIRCHAIN_PORT}657\"|" $HOME/.junction/config/client.toml
+sleep 1
+echo done
 
-sudo wget -O $HOME/.junction/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/main/Airchains/genesis.json
-sudo wget -O $HOME/.junction/config/addrbook.json https://raw.githubusercontent.com/CoinHuntersTR/props/main/Airchains/addrbook.json
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+wget -O $HOME/.junction/config/genesis.json https://testnet-files.itrocket.net/airchains/genesis.json
+wget -O $HOME/.junction/config/addrbook.json https://testnet-files.itrocket.net/airchains/addrbook.json
+sleep 1
+echo done
 
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025amf\"/;" ~/.junction/config/app.toml
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+SEEDS="04e2fdd6ec8f23729f24245171eaceae5219aa91@airchains-testnet-seed.itrocket.net:19656"
+PEERS="47f61921b54a652ca5241e2a7fc4ed8663091e89@airchains-testnet-peer.itrocket.net:19656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.junction/config/config.toml
 
-echo -e "\e[1m\e[32m1. # Pruning... \e[0m"
-pruning="custom"
-pruning_keep_recent="100"
-pruning_keep_every="0"
-pruning_interval="10"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.junction/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.junction/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.junction/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.junction/config/app.toml
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${AIRCHAIN_PORT}317%g;
+s%:8080%:${AIRCHAIN_PORT}080%g;
+s%:9090%:${AIRCHAIN_PORT}090%g;
+s%:9091%:${AIRCHAIN_PORT}091%g;
+s%:8545%:${AIRCHAIN_PORT}545%g;
+s%:8546%:${AIRCHAIN_PORT}546%g;
+s%:6065%:${AIRCHAIN_PORT}065%g" $HOME/.junction/config/app.toml
 
-echo -e "\e[1m\e[32m1. Set Peers... \e[0m"
-PEERS=$(curl -sS https://junction-rpc.dymion.cloud/net_info | \
-jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | \
-awk -F ':' '{printf "%s:%s%s", $1, $(NF), NR==NF?"":","}')
-echo "$PEERS"
 
-sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.junction/config/config.toml
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${AIRCHAIN_PORT}658%g;
+s%:26657%:${AIRCHAIN_PORT}657%g;
+s%:6060%:${AIRCHAIN_PORT}060%g;
+s%:26656%:${AIRCHAIN_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${AIRCHAIN_PORT}656\"%;
+s%:26660%:${AIRCHAIN_PORT}660%g" $HOME/.junction/config/config.toml
 
-echo -e "\e[1m\e[32m1. Snapshot indiriliyor... \e[0m"
-junctiond tendermint unsafe-reset-all --home ~/.junction/ --keep-addr-book
-curl https://files.dymion.cloud/junction/data.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.junction
+# config pruning
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.junction/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.junction/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.junction/config/app.toml
 
-echo -e "\e[1m\e[32m1. Servis oluşturuluyor... \e[0m"
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.001amf"|g' $HOME/.junction/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.junction/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.junction/config/config.toml
+sleep 1
+echo done
+
+# create service file
 sudo tee /etc/systemd/system/junctiond.service > /dev/null <<EOF
 [Unit]
-Description=junction
+Description=airchains node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which junctiond) start
-Restart=always
-RestartSec=3
+WorkingDirectory=$HOME/.junction
+ExecStart=$(which junctiond) start --home $HOME/.junction
+Restart=on-failure
+RestartSec=5
 LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
+
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+# reset and download snapshot
+junctiond tendermint unsafe-reset-all --home $HOME/.junction
+if curl -s --head curl https://snapshots-testnet.nodejumper.io/airchains-testnet/airchains-testnet_latest.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://snapshots-testnet.nodejumper.io/airchains-testnet/airchains-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.junction
+    else
+  echo no have snap
+fi
+
+# enable and start service
 sudo systemctl daemon-reload
 sudo systemctl enable junctiond
-
-echo -e "\e[1m\e[32m1.Log kontrolü... \e[0m"
-sudo systemctl restart junctiond && journalctl -u junctiond -f -o cat
+sudo systemctl restart junctiond && sudo journalctl -u junctiond -f
