22161a281aba113ac40959923b8738d7cf87b585
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-07-31 22:32:05 +0300

    Create warden.sh

---
+++ AutoInstall/warden.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/common.sh)
+
+printLogo
+
+read -p "Enter WALLET name:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (for example 17, default port=26):" PORT
+echo 'export PORT='$PORT
+
+# set vars
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export WARDEN_CHAIN_ID="buenavista-1"" >> $HOME/.bash_profile
+echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$WARDEN_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$WARDEN_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.21.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+
+
+printGreen "5. Configuring and init app..." && sleep 1
+# config and init app
+wardend init $MONIKER
+sed -i -e "s|^node *=.*|node = \"tcp://localhost:${WARDEN_PORT}657\"|" $HOME/.warden/config/client.toml
+sleep 1
+echo done
+
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+wget -O $HOME/.warden/config/genesis.json https://server-4.itrocket.net/testnet/warden/genesis.json
+wget -O $HOME/.warden/config/addrbook.json  https://server-4.itrocket.net/testnet/warden/addrbook.json
+sleep 1
+echo done
+
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+SEEDS="8288657cb2ba075f600911685670517d18f54f3b@warden-testnet-seed.itrocket.net:18656"
+PEERS="b14f35c07c1b2e58c4a1c1727c89a5933739eeea@warden-testnet-peer.itrocket.net:18656,dc0122e37c203dec43306430a1f1879650653479@37.27.97.16:26656,ab510acb34d4db1c925742f46b6a09fb7e3052bf@116.202.233.2:27356,ac16d7fa01abcc167d25641938b4cf419a0676ae@149.202.79.19:15656,e8b15bf7b20791665d8be9e530aafa3aa80f79c6@168.119.10.134:29474,db6947c73751a64b81360e2487c85c54ec0c81a5@81.17.97.89:656,1885f024590d7cc31ee54217d6626cc71369c287@158.220.108.184:11656,b52d56d1d6b653be8e5215b85846fe65ee55af19@89.58.36.209:5656,e8b4153ae30f47d1ff0912b035b63be8f6cea0d9@192.99.9.143:26656,849252edf13621d4ad531c95c26159be6dbfbd51@37.27.115.100:26671,49fb18d457ec1be09de78472e33e5f685706ec6d@195.26.244.134:22656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" \
+       $HOME/.warden/config/config.toml
+
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${WARDEN_PORT}317%g;
+s%:8080%:${WARDEN_PORT}080%g;
+s%:9090%:${WARDEN_PORT}090%g;
+s%:9091%:${WARDEN_PORT}091%g;
+s%:8545%:${WARDEN_PORT}545%g;
+s%:8546%:${WARDEN_PORT}546%g;
+s%:6065%:${WARDEN_PORT}065%g" $HOME/.warden/config/app.toml
+
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${WARDEN_PORT}658%g;
+s%:26657%:${WARDEN_PORT}657%g;
+s%:6060%:${WARDEN_PORT}060%g;
+s%:26656%:${WARDEN_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${WARDEN_PORT}656\"%;
+s%:26660%:${WARDEN_PORT}660%g" $HOME/.warden/config/config.toml
+
+# config pruning
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.warden/config/app.toml
+
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0025uward"|g' $HOME/.warden/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.warden/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.warden/config/config.toml
+sleep 1
+echo done
+
+# create service file
+sudo tee /etc/systemd/system/wardend.service > /dev/null <<EOF
+[Unit]
+Description=warden node
+After=network-online.target
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.warden
+ExecStart=$(which wardend) start --home $HOME/.warden
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+# reset and download snapshot
+wardend tendermint unsafe-reset-all --home $HOME/.warden
+if curl -s --head curl https://server-4.itrocket.net/testnet/warden/warden_2024-07-31_1523933_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-4.itrocket.net/testnet/warden/warden_2024-07-31_1523933_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.warden
+    else
+  echo "no snapshot founded"
+fi
+
+# enable and start service
+sudo systemctl daemon-reload
+sudo systemctl enable wardend
+sudo systemctl restart wardend && sudo journalctl -u wardend -f
