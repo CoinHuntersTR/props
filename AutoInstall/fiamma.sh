c8ce1e5f86436944bb96821f259ee965a40f8c09
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-08-04 16:49:47 +0300

    Update fiamma.sh

--- AutoInstall/fiamma.sh
+++ AutoInstall/fiamma.sh
@@ -28,7 +28,7 @@ sleep 1
 printGreen "1. Installing go..." && sleep 1
 # install go, if needed
 cd $HOME
-VER="1.22.4"
+VER="1.22.3"
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
@@ -46,7 +46,7 @@ printGreen "4. Installing binary..." && sleep 1
 # download binary
 cd $HOME
 rm -rf fiamma
-git clone https://github.com/fiamma-chain/fiamma.git
+git clone https://github.com/fiamma-chain/fiamma
 cd fiamma
 git checkout v0.1.3
 make install
@@ -54,21 +54,26 @@ make install
 printGreen "5. Configuring and init app..." && sleep 1
 # config and init app
 fiammad init $MONIKER --chain-id $FIAMMA_CHAIN_ID
-
+sed -i -e "s|^node *=.*|node = \"tcp://localhost:${FIAMMA_PORT}657\"|" $HOME/.fiamma/config/client.toml
+sed -i -e "s|^keyring-backend *=.*|keyring-backend = \"os\"|" $HOME/.fiamma/config/client.toml
+sed -i -e "s|^chain-id *=.*|chain-id = \"fiamma-testnet-1\"|" $HOME/.fiamma/config/client.toml
+sleep 1
 echo done
 
 printGreen "6. Downloading genesis and addrbook..." && sleep 1
 # download genesis and addrbook
-wget -O $HOME/.fiamma/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/main/fiamma/genesis.json
-wget -O $HOME/.fiamma/config/addrbook.json https://raw.githubusercontent.com/CoinHuntersTR/props/main/fiamma/addrbook.json
+wget -O $HOME/.fiamma/config/genesis.json https://server-5.itrocket.net/testnet/fiamma/genesis.json
+wget -O $HOME/.fiamma/config/addrbook.json  https://server-5.itrocket.net/testnet/fiamma/addrbook.json
 sleep 1
 echo done
 
 printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
 # set seeds and peers
-SEEDS="7f3988dc1f6254e664119d24b52982031e34327b@35.73.202.182:26656,40449ad696760c0d1b675c2741e846b5d08235a3@18.182.20.173:26656"
-PEERS="5d6828849a45cf027e035593d8790bc62aca9cef@18.182.20.173:26656,526d13f3ce3e0b56fa3ac26a48f231e559d4d60c@35.73.202.182:26656,cedc2b0f16422718c320d17fc44935ad1c39e62d@172.31.26.39:26656,74cee55ba0696fcd75d637d0de637b7dfecb67bf@65.109.50.163:12656,21a5cae23e835f99735798024eef39fa0875bc62@65.109.30.110:17456,1833b283cbd1240e5a78c394f2d0955794e7732b@146.19.24.175:26856,4d56ef9164999825b886d67c95d9efbc12b455e9@65.109.49.47:29656,534bbf0712baf6665c2f3793131f7f53aa2806a6@193.70.47.69:26656,47c87d8ac9709669f7e9e9d9c8f5b76118763a13@94.130.216.221:26656,4ccfdc1ae7a8b87a83c0a675932960b750ea0e24@144.76.92.22:11656,4839dd83edd6d7cbb50974d4b1d748104ac56e58@65.109.112.148:40056,f1e941fa754357115f491dd1e138ac70610ab4a4@5.9.87.231:56656,9c87bf6872f2ca15c5f0b73348e6315be512aaa8@65.108.10.239:60956,781ff5ecf63b74c8b3934274ae3d4827ea5c4f74@65.109.57.212:29656,37e2b149db5558436bd507ecca2f62fe605f92fe@88.198.27.51:60556,67fffd28af7cc2a928c52fae6a09fe2812a6638d@217.66.20.45:26656,e00ecc7687e29f09f694dd4dd4a21988ce6a43f9@178.205.102.224:26656,a12e8531f345ccff39f47847aabf12e73e216ee3@144.76.97.251:26796,e2b57b310a6f3c4c0f85fc3dc3447d7e9696cd65@95.165.89.222:26706"
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.fiamma/config/config.toml
+SEEDS="1e8777199f1edb3a35937e653b0bb68422f3c931@fiamma-testnet-seed.itrocket.net:50656"
+PEERS="16b7389e724cc440b2f8a2a0f6b4c495851934ff@fiamma-testnet-peer.itrocket.net:49656,74ec322e114b6757ac066a7b6b55cd224cdb8885@65.21.167.216:37656,37e2b149db5558436bd507ecca2f62fe605f92fe@88.198.27.51:60556,e30701492127fdd86ccf243a55b9dc4146772235@213.199.42.85:37656,e2b57b310a6f3c4c0f85fc3dc3447d7e9696cd65@95.165.89.222:26706,421beadda6355465be81703fd8d25c30b2233df0@5.78.71.69:26656,21a5cae23e835f99735798024eef39fa0875bc62@65.109.30.110:17456,30ae57350193d9491d9a77cfbc3af3908887c7b0@84.247.136.53:26656,043da1f559e0f83eff52ff65f76b012f0f0ee9b3@198.7.119.198:37656,5a6bdb09c087012e9aa9bbdaa95694a82d489a94@144.76.155.11:26856,a03a1a53fafb669bfcce53b8b2a1362aa153cf99@77.90.13.137:37656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" \
+       $HOME/.fiamma/config/config.toml
 
 # set custom ports in app.toml
 sed -i.bak -e "s%:1317%:${FIAMMA_PORT}317%g;
@@ -101,7 +106,7 @@ sleep 1
 echo done
 
 # create service file
-sudo tee /etc/systemd/system/fiamma.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/fiammad.service > /dev/null <<EOF
 [Unit]
 Description=fiamma node
 After=network-online.target
@@ -116,7 +121,16 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+# reset and download snapshot
+fiammad tendermint unsafe-reset-all --home $HOME/.fiamma
+if curl -s --head curl https://server-5.itrocket.net/testnet/fiamma/fiamma_2024-08-04_421972_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-5.itrocket.net/testnet/fiamma/fiamma_2024-08-04_421972_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.fiamma
+    else
+  echo "no snapshot founded"
+fi
+
 # enable and start service
 sudo systemctl daemon-reload
-sudo systemctl enable fiamma.service
-sudo systemctl restart fiamma.service && sudo journalctl -u fiamma.service -f
+sudo systemctl enable fiammad
+sudo systemctl restart fiammad && sudo journalctl -u fiammad -f
