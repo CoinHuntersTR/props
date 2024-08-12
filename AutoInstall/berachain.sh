7c34799c3d4f315d51e6d88bf7ffba356147ba3e
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-08-12 10:43:14 +0300

    Create berachain.sh

---
+++ AutoInstall/berachain.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/common.sh)
+
+printLogo
+
+read -p "Enter WALLET name:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (for example 17, default port=26):" PORT
+echo 'export PORT='$PORT
+
+# set vars
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export SUNRISE_CHAIN_ID="sunrise-test-0.1"" >> $HOME/.bash_profile
+echo "export SUNRISE_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$SUNRISE_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$SUNRISE_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.22.1"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/dependencies_install.sh)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+cd $HOME
+wget -O sunrised wget https://github.com/sunriselayer/sunrise/releases/download/v0.1.2/sunrised
+chmod +x $HOME/sunrised
+mv $HOME/sunrised $HOME/go/bin/sunrised
+
+
+printGreen "5. Configuring and init app..." && sleep 1
+# config and init app
+sunrised init $MONIKER --chain-id $SUNRISE_CHAIN_ID
+sed -i \
+  -e 's/timeout_commit = ".*"/timeout_commit = "30s"/g' \
+  -e 's/timeout_propose = ".*"/timeout_propose = "1s"/g' \
+  -e 's/timeout_precommit = ".*"/timeout_precommit = "1s"/g' \
+  -e 's/timeout_precommit_delta = ".*"/timeout_precommit_delta = "500ms"/g' \
+  -e 's/timeout_prevote = ".*"/timeout_prevote = "1s"/g' \
+  -e 's/timeout_prevote_delta = ".*"/timeout_prevote_delta = "500ms"/g' \
+  -e 's/timeout_propose_delta = ".*"/timeout_propose_delta = "500ms"/g' \
+  -e 's/skip_timeout_commit = ".*"/skip_timeout_commit = false/g' \
+  $HOME/.sunrise/config/config.toml
+sleep 1
+echo done
+
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+wget -O $HOME/.sunrise/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/main/sunrise/genesis.json
+sleep 1
+echo done
+
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+SEEDS="27d92a62f64585b995a6a99882cdd3a1a441336d@a.sunrise-test-1.cauchye.net:26656,9cb96bd137c6fab41446f5fa36e17b1f8896b05c@b.sunrise-test-1.cauchye.net:26656,db223ecc4fba0e7135ba782c0fd710580c5213a6@a-node.sunrise-test-1.cauchye.net:26656"
+PEERS=""
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.sunrise/config/config.toml
+
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${SUNRISE_PORT}317%g;
+s%:8080%:${SUNRISE_PORT}080%g;
+s%:9090%:${SUNRISE_PORT}090%g;
+s%:9091%:${SUNRISE_PORT}091%g;
+s%:8545%:${SUNRISE_PORT}545%g;
+s%:8546%:${SUNRISE_PORT}546%g;
+s%:6065%:${SUNRISE_PORT}065%g" $HOME/.sunrise/config/app.toml
+
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${SUNRISE_PORT}658%g;
+s%:26657%:${SUNRISE_PORT}657%g;
+s%:6060%:${SUNRISE_PORT}060%g;
+s%:26656%:${SUNRISE_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${SUNRISE_PORT}656\"%;
+s%:26660%:${SUNRISE_PORT}660%g" $HOME/.sunrise/config/config.toml
+
+# config pruning
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.sunrise/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.sunrise/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.sunrise/config/app.toml
+
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0urise"|g' $HOME/.sunrise/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.sunrise/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.sunrise/config/config.toml
+sleep 1
+echo done
+
+# create service file
+sudo tee /etc/systemd/system/sunrised.service > /dev/null <<EOF
+[Unit]
+Description=sunrise node
+After=network-online.target
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.sunrise
+ExecStart=$(which sunrised) start --home $HOME/.sunrise
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# enable and start service
+sudo systemctl daemon-reload
+sudo systemctl enable sunrised
+sudo systemctl restart sunrised && sudo journalctl -u sunrised -f
