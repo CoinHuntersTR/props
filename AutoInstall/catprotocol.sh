95729193cbf4d8b007508d72d11e8b289bf7bfce
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-09-12 21:46:01 +0300

    Update catprotocol.sh

--- AutoInstall/catprotocol.sh
+++ AutoInstall/catprotocol.sh
@@ -10,52 +10,52 @@ echo " \____\___/|_|_| |_| |_| |_|\__,_|_| |_|\__\___|_|  |___/ "
 echo -e "\e[0m"
 
 # Update package list and install Docker
-echo "Paket listesini güncelliyor ve Docker'ı yüklüyor..."
+echo -e "\033[0;32mPaket listesini güncelliyor ve Docker'ı yüklüyor...\033[0m"
 sudo apt-get update
 sudo apt-get install docker.io -y
 
 # Install Docker Compose
-echo "Docker Compose'u yüklüyor..."
+echo -e "\033[0;32mDocker Compose'u yüklüyor...\033[0m"
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
 DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 sudo chmod 755 $DESTINATION
 
 # Install Node.js and Yarn using NodeSource
-echo "Node.js ve Yarn'ı yüklüyor..."
+echo -e "\033[0;32mNode.js ve Yarn'ı yüklüyor...\033[0m"
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g yarn
 
 # Clone the CAT Protocol repository
-echo "CAT Protocol deposunu klonluyor..."
+echo -e "\033[0;32mCAT Protocol deposunu klonluyor...\033[0m"
 git clone https://github.com/CATProtocol/cat-token-box
 cd cat-token-box
 
 # Install dependencies and build the project
-echo "Bağımlılıkları yüklüyor ve projeyi derliyor..."
+echo -e "\033[0;32mBağımlılıkları yüklüyor ve projeyi derliyor...\033[0m"
 yarn install
 yarn build
 
 # Update the tracker URL in config.json
 CONFIG_FILE="./packages/cli/config.json"
 if [ -f "$CONFIG_FILE" ]; then
-    echo "config.json dosyasını güncelliyor..."
+    echo -e "\033[0;32mconfig.json dosyasını güncelliyor...\033[0m"
     sed -i 's|"tracker": "http://127.0.0.1:3000"|"tracker": "http://162.55.47.20:3000"|' $CONFIG_FILE
 else
-    echo "config.json dosyası bulunamadı. Lütfen dosyanın varlığını kontrol edin."
+    echo -e "\033[0;31mconfig.json dosyası bulunamadı. Lütfen dosyanın varlığını kontrol edin.\033[0m"
     exit 1
 fi
 
 # Set permissions and start Docker Compose
-echo "İzinleri ayarlıyor ve Docker Compose'u başlatıyor..."
+echo -e "\033[0;32mİzinleri ayarlıyor ve Docker Compose'u başlatıyor...\033[0m"
 cd ./packages/tracker/
 sudo chmod 777 docker/data
 sudo chmod 777 docker/pgdata
 docker-compose up -d
 
 # Build and run the Docker container
-echo "Docker konteynerini oluşturuyor ve çalıştırıyor..."
+echo -e "\033[0;32mDocker konteynerini oluşturuyor ve çalıştırıyor...\033[0m"
 cd ../../
 docker build -t tracker:latest .
 docker run -d \
@@ -67,11 +67,11 @@ docker run -d \
     tracker:latest
 
 # Create a wallet
-echo "Cüzdanınız oluşturuluyor. Gizli kelimelerinizi saklamayı unutmayın."
+echo -e "\033[0;32mCüzdanınız oluşturuluyor. Gizli kelimelerinizi saklamayı unutmayın.\033[0m"
 cd packages/cli
 yarn build
 yarn cli wallet create
 
 # Display wallet address
-echo "Cüzdan adresiniz. FB coin göndermeyi unutmayın."
+echo -e "\033[0;32mCüzdan adresiniz. FB coin göndermeyi unutmayın.\033[0m"
 yarn cli wallet address
