f79775dad6e555d0941676468ca910bf196a9a45
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-10-29 23:25:30 +0300

    Create story-odyssey.sh

---
+++ AutoInstall/story-odyssey.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/common.sh)
+
+printLogo
+
+
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (for example 17, default port=26):" PORT
+echo 'export PORT='$PORT
+
+# set vars
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export STORY_CHAIN_ID="odyssey"" >> $HOME/.bash_profile
+echo "export STORY_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$STORY_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$STORY_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.22.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/refs/heads/main/dependencies_install.sh)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+cd $HOME
+wget -O geth https://github.com/piplabs/story-geth/releases/download/v0.10.0/geth-linux-amd64
+chmod +x $HOME/geth
+mv $HOME/geth ~/go/bin/
+[ ! -d "$HOME/.story/story" ] && mkdir -p "$HOME/.story/story"
+[ ! -d "$HOME/.story/geth" ] && mkdir -p "$HOME/.story/geth"
+
+# install Story
+cd $HOME
+rm -rf story
+git clone https://github.com/piplabs/story
+cd story
+git checkout v0.12.0
+go build -o story ./client 
+mv $HOME/story/story $HOME/go/bin/
+
+
+
+printGreen "5. Configuring and init app..." && sleep 1
+
+# init story app
+story init $MONIKER --network odyssey
+
+
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+# download genesis and addrbook
+wget -O $HOME/.story/story/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/story/genesis.json
+wget -O $HOME/.story/story/config/addrbook.json  https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/story/addrbook.json
+
+sleep 1
+echo done
+
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+# set seeds and peers
+URL="https://story-testnet-rpc.itrocket.net/net_info
+response=$(curl -s $URL)
+PEERS=$(echo $response | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):" + (.node_info.listen_addr | capture("(?<ip>.+):(?<port>[0-9]+)$").port)' | paste -sd "," -)
+echo "PEERS=\"$PEERS\""
+
+# Update the persistent_peers in the config.toml file
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|; s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/.story/story/config/config.toml
+
+
+# set custom ports in story.toml file
+sed -i.bak -e "s%:1317%:${STORY_PORT}317%g;
+s%:8551%:${STORY_PORT}551%g" $HOME/.story/story/config/story.toml
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${STORY_PORT}658%g;
+s%:26657%:${STORY_PORT}657%g;
+s%:26656%:${STORY_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${STORY_PORT}656\"%;
+s%:26660%:${STORY_PORT}660%g" $HOME/.story/story/config/config.toml
+
+# enable prometheus and disable indexing
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.story/story/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.story/story/config/config.toml
+
+# create geth servie file
+sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
+[Unit]
+Description=Story Geth daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/geth --odyssey --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port ${STORY_PORT}545 --authrpc.port ${STORY_PORT}551 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port ${STORY_PORT}546
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# create story service file
+sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
+[Unit]
+Description=Story Service
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.story/story
+ExecStart=$(which story) run
+
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# download snapshots
+# backup priv_validator_state.json
+cp $HOME/.story/story/data/priv_validator_state.json $HOME/.story/story/priv_validator_state.json.backup
+
+# remove old data and unpack Story snapshot
+rm -rf $HOME/.story/story/data
+curl https://snapshots.coinhunterstr.com/story/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.story/story
+
+# restore priv_validator_state.json
+mv $HOME/.story/story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
+
+# delete geth data and unpack Geth snapshot
+rm -rf $HOME/.story/geth/odyssey/geth/chaindata
+mkdir -p $HOME/.story/geth/odyssey/geth
+curl https://snapshots.coinhunterstr.com/story-geth/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.story/geth/odyssey/geth
+
+# enable and start geth, story
+sudo systemctl daemon-reload
+sudo systemctl enable story story-geth
+sudo systemctl restart story-geth && sleep 5 && sudo systemctl restart story
+
+# check logs
+journalctl -u story -u story-geth -f
