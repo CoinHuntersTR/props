a2bd8f6ac866c85ea23af32fec9e68f0fee31108
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-10-30 11:39:28 +0300

    Update pellnetwork.sh

--- AutoInstall/pellnetwork.sh
+++ AutoInstall/pellnetwork.sh
@@ -13,15 +13,15 @@ echo 'export PORT='$PORT
 # set vars
 echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
 echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
-echo "export WARDEN_CHAIN_ID="athens_186-1"" >> $HOME/.bash_profile
-echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
+echo "export PELL_CHAIN_ID="athens_186-1"" >> $HOME/.bash_profile
+echo "export PELL_PORT="$PORT"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 printLine
 echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
 echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
-echo -e "Chain id:       \e[1m\e[32m$WARDEN_CHAIN_ID\e[0m"
-echo -e "Node custom port:  \e[1m\e[32m$WARDEN_PORT\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$PELL_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$PELL_PORT\e[0m"
 printLine
 sleep 1
 
@@ -45,77 +45,70 @@ source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/refs/heads
 printGreen "4. Installing binary..." && sleep 1
 # download binary
 cd $HOME
-rm -rf bin
-mkdir bin && cd bin
-wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.5.2/wardend_Linux_x86_64.zip
-unzip wardend_Linux_x86_64.zip
-chmod +x wardend
-mv $HOME/bin/wardend $HOME/go/bin
+wget https://snapshots.coinhunterstr.com/pellnetwork/pellcored
+chmod +x $HOME/pellcored
+mv $HOME/pellcored $HOME/go/bin/pellcored
 
 printGreen "5. Configuring and init app..." && sleep 1
 # config and init app
-wardend init $MONIKER
-sed -i -e "s|^node *=.*|node = \"tcp://localhost:${WARDEN_PORT}657\"|" $HOME/.warden/config/client.toml
+pellcored init $MONIKER
+sed -i -e "s|^node *=.*|node = \"tcp://localhost:${PELL_PORT}657\"|" $HOME/.pellcored/config/client.toml
 sleep 1
 echo done
 
 printGreen "6. Downloading genesis and addrbook..." && sleep 1
 # download genesis and addrbook
-wget -O $HOME/.warden/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/warden/Chiado/genesis.json
-wget -O $HOME/.warden/config/addrbook.json  https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/warden/Chiado/addrbook.json
+wget -O $HOME/.pellcored/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/pellnetwork/genesis.json
+wget -O $HOME/.pellcored/config/addrbook.json  https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/pellnetwork/addrbook.json
 sleep 1
 echo done
 
 printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
 # set seeds and peers
-# set seeds and peers
-URL="https://warden-testnet-rpc.itrocket.net/net_info"
-response=$(curl -s $URL)
-PEERS=$(echo $response | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):" + (.node_info.listen_addr | capture("(?<ip>.+):(?<port>[0-9]+)$").port)' | paste -sd "," -)
-echo "PEERS=\"$PEERS\""
-
-# Update the persistent_peers in the config.toml file
-sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|; s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/.warden/config/config.toml
+SEEDS="e3abea7feb928a0470dfb06962213a2fb48bbbed@57.180.60.57:26656"
+PEERS="e3abea7feb928a0470dfb06962213a2fb48bbbed@57.180.60.57:26656,3f5e2e7b2e5acb20bd9248875121ee5828f1b8d3@185.252.235.216:26656,5a95d9f89d3484c439a357bc7c276dd88f19ebbf@37.252.186.230:26656,8283c7a961c40223c6b8b152c3d7b93987336dbe@147.135.78.73:26656,eb58c53b359bfcace5898e4dd1a00c1c9d2175f4@34.44.209.45:26656,c5670c134030edf72493aad1591284aaa0ebc97c@171.226.129.85:26656,f4f97e7620c6bfa22574c538785a106053a25640@65.109.53.24:26656,8533ff42e9858caa386f3956433e0194278a3240@188.40.85.207:26656,0e176a983bad2d6c2ae216bc46b4315f9da35054@94.130.204.227:26656,2718def8e804cfef7ee5df4d83eba23f1fb9793e@194.163.133.231:26656,a6c7a522537b2bb2a9b946c1e6bc7f6a78731192@103.164.81.233:26656,15f92c0b31adf02d46de02610221ff3ddecfb80e@167.235.21.165:56656,d12f3b1680d1179322bbf5096d395ee3db496e33@195.14.6.169:26656,c7be87435d823439fe2261eab0e053bafc7a7459@74.50.91.170:26656,3599ca9ec1432d2e5386c12cd442648ff7b2e543@62.171.130.196:26656,1f365d37e0de430823a12b2d0558f938e7ee7151@65.21.28.38:26656,b7c36c14c1a1971ead5436e696902b6e869dbcd0@43.153.154.247:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.pellcored/config/config.toml
 
 # set custom ports in app.toml
-sed -i.bak -e "s%:1317%:${WARDEN_PORT}317%g;
-s%:8080%:${WARDEN_PORT}080%g;
-s%:9090%:${WARDEN_PORT}090%g;
-s%:9091%:${WARDEN_PORT}091%g;
-s%:8545%:${WARDEN_PORT}545%g;
-s%:8546%:${WARDEN_PORT}546%g;
-s%:6065%:${WARDEN_PORT}065%g" $HOME/.warden/config/app.toml
+sed -i.bak -e "s%:1317%:${PELL_PORT}317%g;
+s%:8080%:${PELL_PORT}080%g;
+s%:9090%:${PELL_PORT}090%g;
+s%:9091%:${PELL_PORT}091%g;
+s%:8545%:${PELL_PORT}545%g;
+s%:8546%:${PELL_PORT}546%g;
+s%:6065%:${PELL_PORT}065%g" $HOME/.pellcored/config/app.toml
 
 
 # set custom ports in config.toml file
-sed -i.bak -e "s%:26658%:${WARDEN_PORT}658%g;
-s%:26657%:${WARDEN_PORT}657%g;
-s%:6060%:${WARDEN_PORT}060%g;
-s%:26656%:${WARDEN_PORT}656%g;
-s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${WARDEN_PORT}656\"%;
-s%:26660%:${WARDEN_PORT}660%g" $HOME/.warden/config/config.toml
+sed -i.bak -e "s%:26658%:${PELL_PORT}658%g;
+s%:26657%:${PELL_PORT}657%g;
+s%:6060%:${PELL_PORT}060%g;
+s%:26656%:${PELL_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${PELL_PORT}656\"%;
+s%:26660%:${PELL_PORT}660%g" $HOME/.pellcored/config/config.toml
 
 # config pruning
-sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.warden/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.warden/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME.pellcored/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.pellcored/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.pellcored/config/app.toml
 
 # set minimum gas price, enable prometheus and disable indexing
-sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "25000000award"|g' $HOME/.warden/config/app.toml
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.warden/config/config.toml
-sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.warden/config/config.toml
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0apell"|g' $HOME/.pellcored/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.pellcored/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.pellcored/config/config.toml
 sleep 1
 echo done
 
 # create service file
-sudo tee /etc/systemd/system/wardend.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/pellcored.service > /dev/null <<EOF
 [Unit]
-Description=warden node
+Description=pellnetwork node
 After=network-online.target
 [Service]
 User=$USER
-WorkingDirectory=$HOME/.warden
-ExecStart=$(which wardend) start --home $HOME/.warden
+WorkingDirectory=$HOME/.pellcored
+ExecStart=$(which pellcored) start --home $HOME/.pellcored
 Restart=on-failure
 RestartSec=5
 LimitNOFILE=65535
@@ -123,24 +116,8 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-printGreen "8. Downloading snapshot and starting node..." && sleep 1
-# reset and download snapshot
-wardend tendermint unsafe-reset-all --home $HOME/.warden
-BASE_URL="https://server-4.itrocket.net/testnet/warden/"
-LATEST_SNAPSHOT=$(curl -s $BASE_URL | grep -oP 'warden_\d{4}-\d{2}-\d{2}_\d+_snap\.tar\.lz4' | sort -V | tail -n 1)
-
-if [ -n "$LATEST_SNAPSHOT" ]; then
-  FULL_URL="${BASE_URL}${LATEST_SNAPSHOT}"
-  if curl -s --head "$FULL_URL" | head -n 1 | grep "200" > /dev/null; then
-    curl "$FULL_URL" | lz4 -dc - | tar -xf - -C $HOME/.warden
-  else
-    echo "Snapshot URL is not valid."
-  fi
-else
-  echo "No snapshot found."
-fi
 
 # enable and start service
 sudo systemctl daemon-reload
-sudo systemctl enable wardend
-sudo systemctl restart wardend && sudo journalctl -u wardend -f
+sudo systemctl enable pellcored
+sudo systemctl restart pellcored && sudo journalctl -u pellcored -f
