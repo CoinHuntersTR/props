220d9bda425d29dfe4df56b09f45475f2fe7bbd7
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-11-11 21:13:05 +0300

    Update mantrachain.sh

--- AutoInstall/mantrachain.sh
+++ AutoInstall/mantrachain.sh
@@ -45,9 +45,9 @@ source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/refs/heads
 printGreen "4. Installing binary..." && sleep 1
 # download binary
 cd $HOME
-wget -O mantrachaind-1.0.0-rc2-linux-amd64.tar.gz https://github.com/MANTRA-Chain/mantrachain/releases/download/v1.0.0-rc2/mantrachaind-1.0.0-rc2-linux-amd64.tar.gz
-tar -xzf mantrachaind-1.0.0-rc2-linux-amd64.tar.gz
-rm $HOME/mantrachaind-1.0.0-rc2-linux-amd64.tar.gz
+wget -O mantrachaind-1.0.2-linux-amd64.tar.gz https://github.com/MANTRA-Chain/mantrachain/releases/download/v1.0.2/mantrachaind-1.0.2-linux-amd64.tar.gz
+tar -xzf mantrachaind-1.0.2-linux-amd64.tar.gz
+rm $HOME/mantrachaind-1.0.2-linux-amd64.tar.gz
 chmod +x $HOME/mantrachaind
 sudo mv $HOME/mantrachaind $HOME/go/bin/mantrachaind
 
