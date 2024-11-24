0d6aa9f145748207402c12ea1be2c72d02257e85
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-11-24 21:44:16 +0300

    Update node_installer.sh

--- AutoInstall/node_installer.sh
+++ AutoInstall/node_installer.sh
@@ -13,15 +13,15 @@ clear
 # Banner
 echo -e "${BLUE}╔════════════════════════════════════════════════════════════╗${NC}"
 echo -e "${BLUE}║                                                            ║${NC}"
-echo -e "${BLUE}║${GREEN}                 Node Kurulum Scripti                      ${BLUE}║${NC}"
-echo -e "${BLUE}║${YELLOW}                    by CoinHuntersTR                      ${BLUE}║${NC}"
+echo -e "${BLUE}║${GREEN}                 Node Kurulum Scripti        ${BLUE}║${NC}"
+echo -e "${BLUE}║${YELLOW}                    by CoinHuntersTR        ${BLUE}║${NC}"
 echo -e "${BLUE}║                                                            ║${NC}"
 echo -e "${BLUE}╚════════════════════════════════════════════════════════════╝${NC}"
 
 while true; do
   echo -e "\n${GREEN}Lütfen ağ türünü seçin:${NC}"
-  echo -e "1) Mainnet"
-  echo -e "2) Testnet"
+  echo -e "1) Mainnet Chain Kurulumu"
+  echo -e "2) Testnet Chain Kurulumu"
   echo -e "3) Çıkış"
 
   read -p "Seçiminiz (1-3): " network_choice
@@ -31,10 +31,15 @@ while true; do
           echo -e "\n${BLUE}Mainnet Projeleri:${NC}"
           echo "1) Dymension"
           echo "2) Lava Network"
-          echo "3) Zetachain"
-          echo "4) Geri"
+          echo "3) Mantra Chain"
+          echo "4) Nibiru Chain"          
+          echo "5) Zeta Chain"
+          echo "6) CrossFi" 
+          echo "7) Axelar"
+          echo "8) Osmosis"           
+          echo "9) Geri"
 
-          read -p "Proje seçin (1-4): " mainnet_choice
+          read -p "Proje seçin (1-9): " mainnet_choice
 
           case $mainnet_choice in
               1)
@@ -46,10 +51,30 @@ while true; do
                   bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/lava.sh)
                   ;;
               3)
-                  echo "Zetachain kurulumu başlatılıyor..."
-                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/zetachain.sh)
+                  echo "Mantra Chain kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/MantraMainnet.sh)
                   ;;
               4)
+                  echo "Nibiru Chain kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/nibiru.sh)
+                  ;;    
+              5)
+                  echo "Zeta Chain kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/zetachain.sh)
+                  ;;
+              6)
+                  echo "CrossFi kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/AutoInstall/crossfi.sh)
+                  ;;
+              7)
+                  echo "Axelar kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/AutoInstall/axelar.sh)
+                  ;;
+               8)
+                  echo "Osmosis kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/AutoInstall/osmosis.sh)
+                  ;;                 
+              9)
                   continue
                   ;;
               *)
@@ -61,11 +86,17 @@ while true; do
       2)
           echo -e "\n${BLUE}Testnet Projeleri:${NC}"
           echo "1) Story Protocol"
-          echo "2) MantraChain"
-          echo "3) Warden"
-          echo "4) Geri"
+          echo "2) Mantra Chain"
+          echo "3) Warden Protocol"
+          echo "4) Airchains"
+          echo "5) Side Protocol"
+          echo "6) Sunrise"
+          echo "7) Union"
+          echo "8) Empeiria"
+          echo "9) Axone"      
+          echo "10) Geri"
 
-          read -p "Proje seçin (1-4): " testnet_choice
+          read -p "Proje seçin (1-10): " testnet_choice
 
           case $testnet_choice in
               1)
@@ -73,14 +104,38 @@ while true; do
                   bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/story.sh)
                   ;;
               2)
-                  echo "MantraChain kurulumu başlatılıyor..."
-                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/mantra.sh)
+                  echo "Mantra Chain kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/mantrachain.sh)
                   ;;
               3)
-                  echo "Warden kurulumu başlatılıyor..."
+                  echo "Warden Protocol kurulumu başlatılıyor..."
                   bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/warden.sh)
                   ;;
               4)
+                  echo "Airchains kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/Airchains.sh)
+                  ;;
+              5)
+                  echo "Side Protocol kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/side.sh)
+                  ;;
+              6)
+                  echo "Sunrise kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/sunrise.sh)
+                  ;;
+              7)
+                  echo "Union kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/union.sh)
+                  ;;
+              8)
+                  echo "Empeiria kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/empeiria.sh)
+                  ;;
+              9)
+                  echo "Axone kurulumu başlatılıyor..."
+                  bash <(wget -qO- https://raw.githubusercontent.com/CoinHuntersTR/props/main/AutoInstall/axone.sh)
+                  ;;                  
+              10)
                   continue
                   ;;
               *)
