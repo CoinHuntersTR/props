72bd1b3119f42dc6709b12319b5228e5968172e5
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-12-13 08:37:31 +0300

    Update pellnetwork.sh

--- AutoInstall/pellnetwork.sh
+++ AutoInstall/pellnetwork.sh
@@ -13,7 +13,7 @@ echo 'export PORT='$PORT
 # set vars
 echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
 echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
-echo "export PELL_CHAIN_ID="athens_186-1"" >> $HOME/.bash_profile
+echo "export PELL_CHAIN_ID="ignite_186-1"" >> $HOME/.bash_profile
 echo "export PELL_PORT="$PORT"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
@@ -45,7 +45,7 @@ source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/refs/heads
 printGreen "4. Installing binary..." && sleep 1
 # download binary
 cd $HOME
-wget https://snapshots.coinhunterstr.com/pellnetwork/pellcored
+wget -O pellcored https://github.com/0xPellNetwork/network-config/releases/download/v1.0.20-ignite/pellcored-v1.0.20-linux-amd64
 chmod +x $HOME/pellcored
 mv $HOME/pellcored $HOME/go/bin/pellcored
 
@@ -64,8 +64,8 @@ echo done
 
 printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
 # set seeds and peers
-SEEDS="e3abea7feb928a0470dfb06962213a2fb48bbbed@57.180.60.57:26656"
-PEERS="e3abea7feb928a0470dfb06962213a2fb48bbbed@57.180.60.57:26656,3f5e2e7b2e5acb20bd9248875121ee5828f1b8d3@185.252.235.216:26656,5a95d9f89d3484c439a357bc7c276dd88f19ebbf@37.252.186.230:26656,8283c7a961c40223c6b8b152c3d7b93987336dbe@147.135.78.73:26656,eb58c53b359bfcace5898e4dd1a00c1c9d2175f4@34.44.209.45:26656,c5670c134030edf72493aad1591284aaa0ebc97c@171.226.129.85:26656,f4f97e7620c6bfa22574c538785a106053a25640@65.109.53.24:26656,8533ff42e9858caa386f3956433e0194278a3240@188.40.85.207:26656,0e176a983bad2d6c2ae216bc46b4315f9da35054@94.130.204.227:26656,2718def8e804cfef7ee5df4d83eba23f1fb9793e@194.163.133.231:26656,a6c7a522537b2bb2a9b946c1e6bc7f6a78731192@103.164.81.233:26656,15f92c0b31adf02d46de02610221ff3ddecfb80e@167.235.21.165:56656,d12f3b1680d1179322bbf5096d395ee3db496e33@195.14.6.169:26656,c7be87435d823439fe2261eab0e053bafc7a7459@74.50.91.170:26656,3599ca9ec1432d2e5386c12cd442648ff7b2e543@62.171.130.196:26656,1f365d37e0de430823a12b2d0558f938e7ee7151@65.21.28.38:26656,b7c36c14c1a1971ead5436e696902b6e869dbcd0@43.153.154.247:26656"
+SEEDS="d52c32a6a8510bdf0d33909008041b96d95c8408@34.87.39.12:26656,9b955d07f05b02b3d622f9cb7a0e6cfecd719985@34.87.47.193:26656"
+PEERS="d52c32a6a8510bdf0d33909008041b96d95c8408@34.87.39.12:26656,9b955d07f05b02b3d622f9cb7a0e6cfecd719985@34.87.47.193:26656"
 sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
        -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.pellcored/config/config.toml
 
