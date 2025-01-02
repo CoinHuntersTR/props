9e5bbfc56e993f897e45b9c4eb398d0b6d7393e8
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2025-01-02 17:25:56 +0300

    Update kopi.sh

--- AutoInstall/kopi.sh
+++ AutoInstall/kopi.sh
@@ -61,6 +61,7 @@ echo done
 printGreen "6. Downloading genesis and addrbook..." && sleep 1
 # download genesis and addrbook
 wget -O $HOME/.kopid/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/kopi/genesis.json
+wget -O $HOME/.kopid/config/addrbook.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/kopi/addrbook.json
 
 sleep 1
 echo done
