881705bab92a565c4ce28628ce6cadac3db1846a
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2025-01-15 01:35:09 +0300

    Update babylon.sh

--- AutoInstall/babylon.sh
+++ AutoInstall/babylon.sh
@@ -26,24 +26,29 @@ printLine
 sleep 1
 
 printGreen "1. Installing go..." && sleep 1
-# install go, if needed
+# install go
 cd $HOME
-VER="1.23.1"
+VER="1.23.4"
+sudo rm -rf /usr/local/go/
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
 rm "go$VER.linux-amd64.tar.gz"
-[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source $HOME/.bash_profile
-[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Configure Go
+echo "export GOROOT=/usr/local/go" >> ~/.bash_profile
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile
+echo "export GO111MODULE=on" >> ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
 
 echo $(go version) && sleep 1
 
+printGreen "2. Installing Cosmovisor..." && sleep 1
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+
 source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
 
-printGreen "4. Installing binary..." && sleep 1
-# download binary
+printGreen "3. Installing babylon binary..." && sleep 1
 cd $HOME
 rm -rf babylon
 git clone https://github.com/babylonlabs-io/babylon.git
@@ -52,26 +57,30 @@ git checkout v1.0.0-rc.3
 make install
 babylond version
 
-printGreen "5. Configuring and init app..." && sleep 1
-# config and init app
+printGreen "4. Configuring and initializing node..." && sleep 1
 babylond init $MONIKER --chain-id $BABYLON_CHAIN_ID
 
-printGreen "6. Downloading genesis and addrbook..." && sleep 1
-# download genesis and addrbook
+printGreen "5. Downloading genesis and addrbook..." && sleep 1
 wget -O $HOME/.babylond/config/genesis.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/babylon/genesis.json
 wget -O $HOME/.babylond/config/addrbook.json https://raw.githubusercontent.com/CoinHuntersTR/props/refs/heads/main/babylon/addrbook.json
 sleep 1
 echo done
 
-printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+printGreen "6. Setting up Cosmovisor..." && sleep 1
+# Create Cosmovisor folders
+mkdir -p ~/.babylond/cosmovisor/genesis/bin
+mkdir -p ~/.babylond/cosmovisor/upgrades
+
+# Copy binary to Cosmovisor folder
+cp ~/go/bin/babylond ~/.babylond/cosmovisor/genesis/bin
+
+printGreen "7. Configuring node..." && sleep 1
 # set seeds and peers
 PEERS="be232be53f7ac3c4a6628f98becb48fd25df1adf@babylon-testnet-seed.nodes.guru:55706,ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:20656"
 URL="https://babylon-testnet-rpc.polkachu.com/net_info"
 response=$(curl -s $URL)
 PEERS=$(echo $response | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):" + (.node_info.listen_addr | capture("(?<ip>.+):(?<port>[0-9]+)$").port)' | paste -sd "," -)
-echo "PEERS=\"$PEERS\""
 
-# Update the persistent_peers in the config.toml file
 sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|; s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/.babylond/config/config.toml
 
 # set custom ports in app.toml
@@ -83,8 +92,7 @@ s%:8545%:${BABYLON_PORT}545%g;
 s%:8546%:${BABYLON_PORT}546%g;
 s%:6065%:${BABYLON_PORT}065%g" $HOME/.babylond/config/app.toml
 
-
-# set custom ports in config.toml file
+# set custom ports in config.toml
 sed -i.bak -e "s%:26658%:${BABYLON_PORT}658%g;
 s%:26657%:${BABYLON_PORT}657%g;
 s%:6060%:${BABYLON_PORT}060%g;
@@ -97,30 +105,41 @@ sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.babylond/config/app.tom
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.babylond/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.babylond/config/app.toml
 
-# set minimum gas price, enable prometheus and disable indexing
+# set minimum gas price and other configs
 sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.002ubbn"|g' $HOME/.babylond/config/app.toml
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.babylond/config/config.toml
 sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.babylond/config/config.toml
 sleep 1
 echo done
 
-# create service file
-sudo tee /etc/systemd/system/babylond.service > /dev/null <<EOF
+printGreen "8. Creating service file..." && sleep 1
+# create service file with Cosmovisor
+sudo tee /etc/systemd/system/babylon.service > /dev/null <<EOF
 [Unit]
-Description=babylond node
+Description="babylon node"
 After=network-online.target
+
 [Service]
 User=$USER
-WorkingDirectory=$HOME/.babylond
-ExecStart=$(which babylond) start --home $HOME/.babylond
-Restart=on-failure
-RestartSec=5
-LimitNOFILE=65535
+ExecStart=$(which cosmovisor) start
+Restart=always
+RestartSec=3
+LimitNOFILE=4096
+Environment="DAEMON_NAME=babylond"
+Environment="DAEMON_HOME=$HOME/.babylond"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="UNSAFE_SKIP_BACKUP=true"
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
+printGreen "9. Starting node..." && sleep 1
 # enable and start service
 sudo systemctl daemon-reload
-sudo systemctl enable babylond
-sudo systemctl restart babylond && sudo journalctl -u babylond -f
+sudo systemctl enable babylon.service
+sudo systemctl start babylon.service
+
+printGreen "10. Node logs:" && sleep 1
+sudo journalctl -fu babylon -o cat
