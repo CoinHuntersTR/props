333175b6febf50b30e2e595853b74e26354a5407
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2025-01-31 13:12:32 +0300

    Update story.sh

--- AutoInstall/story.sh
+++ AutoInstall/story.sh
@@ -103,7 +103,7 @@ After=network.target
 
 [Service]
 User=$USER
-ExecStart=$(which story-geth) --story --syncmode full
+ExecStart=$HOME/go/bin/geth --odyssey --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port ${STORY_PORT}545 --authrpc.port ${STORY_PORT}551 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port ${STORY_PORT}546
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=4096
