7b31aa91ce892f8321bcb02a91608bb66d100a46
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2025-05-05 22:38:34 +0300

    Update aeneid.sh

--- AutoInstall/aeneid.sh
+++ AutoInstall/aeneid.sh
@@ -21,9 +21,9 @@ sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -
 echo "Installing Go..."
 cd $HOME
 VER="1.22.11"
-wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+wget "https://golang.org/dl/go$VER.linux-arm64.tar.gz"
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf "go$VER.linux-arm64.tar.gz"
 rm "go$VER.linux-amd64.tar.gz"
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
 echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
